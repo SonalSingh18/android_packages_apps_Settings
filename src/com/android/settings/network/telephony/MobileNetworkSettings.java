86518dc2d3fc200b66375b13a7376a1441842724
DarkJoker360 <simoespo159@gmail.com>
2023-03-31 10:42:42 +0000

    Settings: Add a toggle to force LTE_CA
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -335,6 +335,7 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings impleme
                 subscriptionPreferenceControllers) {
             controller.init(mSubId);
         }
+        use(ForceLteCaPreferenceController.class).init(mSubId);
     }
 
     @Override
